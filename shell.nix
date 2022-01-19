92e9a8f957ae7d8fd56519821a2c41cc5c28a52a
Luiz Ribeiro <luizribeiro@gmail.com>
2022-01-18 23:22:56 -0600

    Got it to work... I think?

--- shell.nix
+++ shell.nix
@@ -4,8 +4,10 @@ pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     # dependencies
     cargo
+    cmake
     rust-bindgen
     rustc
+    zlib
 
     # tools for development
     rls
--- src/build.rs
+++ src/build.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::process::Command;
+
 fn main() {
-   println!("cargo:rustc-link-lib=leveldb-mcpe");
+    let outdir = env::var("OUT_DIR").unwrap();
+
+    env::set_current_dir("deps/leveldb-mcpe/").expect("Couldn't cd to deps/leveldb-mcpe");
+
+    Command::new("make").status().expect("Make failed");
+    Command::new("cp")
+        .args(&["out-static/libleveldb.a", &outdir])
+        .status()
+        .expect("Failed to copy libleveldb.a to outdir");
+    println!("cargo:rustc-link-search=native={}", outdir);
+    println!("cargo:rustc-link-lib=static=leveldb");
 }
